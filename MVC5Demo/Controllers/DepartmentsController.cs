21b3d16313dcff1f8a8dca514d8b7593efc5eaf0
ERic <n88068@gmail.com>
2020-11-01 10:06:49 +0800

    新增DepartmentsController  Creat 動作

---
+++ MVC5Demo/Controllers/DepartmentsController.cs
@@ -0,0 +1,38 @@
+﻿using MVC5Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Demo.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        ContosoUniversityEntities db = new ContosoUniversityEntities();
+        // GET: Departments
+        public ActionResult Index()
+        {                    
+            return View(db.Department);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Department.Add(department);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorID = new SelectList(db.Person, "ID", "FirstName");
+            return View(department);
+        }
+    }
+}
